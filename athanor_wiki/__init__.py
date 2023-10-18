a97976bc25b3ea013f2af7f7efeb7598cc81cd84
Andrew Bastien <volundmush@gmail.com>
2023-10-18 11:44:25 -0400

    Updating web support.

--- athanor_wiki/__init__.py
+++ athanor_wiki/__init__.py
@@ -1,20 +1,21 @@
 import os
 
-def init(settings, plugins):
 
+def init(settings, plugins):
     THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
-    settings.INSTALLED_APPS.extend([
-        'django.contrib.humanize.apps.HumanizeConfig',
-        'django_nyt.apps.DjangoNytConfig',
-        'mptt',
-        'sorl.thumbnail',
-        'wiki.apps.WikiConfig',
-        'wiki.plugins.attachments.apps.AttachmentsConfig',
-        'wiki.plugins.notifications.apps.NotificationsConfig',
-        'wiki.plugins.images.apps.ImagesConfig',
-        'wiki.plugins.macros.apps.MacrosConfig'
-    ])
+    for app in ('django.contrib.humanize.apps.HumanizeConfig',
+                'django_nyt.apps.DjangoNytConfig',
+                'mptt',
+                'sorl.thumbnail',
+                'wiki.apps.WikiConfig',
+                'wiki.plugins.attachments.apps.AttachmentsConfig',
+                'wiki.plugins.notifications.apps.NotificationsConfig',
+                'wiki.plugins.images.apps.ImagesConfig',
+                'wiki.plugins.macros.apps.MacrosConfig'):
+
+        if app not in settings.INSTALLED_APPS:
+            settings.INSTALLED_APPS.append(app)
 
     settings.WIKI_ACCOUNT_HANDLING = False
     settings.WIKI_ACCOUNT_SIGNUP_ALLOWED = False
